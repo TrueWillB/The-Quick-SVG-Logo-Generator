fdce1718ba91fa9e2d1fda3cab684a6caec86c25
William Brown <willis291@gmail.com>
2023-06-15 21:54:40 -0600

    Added tests

--- index.js
+++ index.js
@@ -82,7 +82,6 @@ function init() {
     const imageOpen =
       '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">\n\n';
     const imageClose = "\n\n</svg>";
-    const textRenderString = `  <text x="150" y="125" font-size="50" font-family="Brush Script MT" text-anchor="middle" fill="${answers.logoTextColor}"> ${answers.logoText}</text>`;
     let svgString = imageOpen;
 
     // console.log(answers);
---
+++ test/renderers.test.js
@@ -0,0 +1,56 @@
+const Renderers = require("../lib/renderers.js");
+
+//test suite for renderers.js
+describe("Renderers", () => {
+  // Testing for correct output of Circle class
+  describe("Circle", () => {
+    it("should take a string for color and put out an svg circle in a string", () => {
+      const color = "yellow";
+      const circle = new Renderers.Circle(color);
+      expect(circle.render()).toEqual(
+        '\n<circle cx=50% cy=50% r=30% fill="yellow" />\n'
+      );
+    });
+  });
+
+  describe("Triangle", () => {
+    it("should take a string for color and put out an svg triangle in a string", () => {
+      const color = "green";
+      const circle = new Renderers.Triangle(color);
+      expect(circle.render()).toEqual(
+        '\n<polygon points="150, 18 244, 182 56, 182" fill="green" />\n'
+      );
+    });
+  });
+
+  describe("Square", () => {
+    it("should take a string for color and put out an svg square in a string", () => {
+      const color = "black";
+      const circle = new Renderers.Square(color);
+      expect(circle.render()).toEqual(
+        '\n<rect x="68" y="18" width="164" height="164" r="90" fill="black" />\n'
+      );
+    });
+  });
+
+  describe("Text", () => {
+    it("should take a string for text and a string for color and put output a text element with those properties in a string", () => {
+      const color = "blue";
+      const text = "test text";
+      const circle = new Renderers.Text(color, text);
+      expect(circle.render()).toEqual(
+        '\n<text x="150" y="125" font-size="50" font-family="Brush Script MT" text-anchor="middle" fill="blue">test text</text>\n'
+      );
+    });
+  });
+
+  describe("BackgroundFill", () => {
+    it("should take a string for color and put out an svg rectangle string", () => {
+      const color = "magenta";
+      const circle = new Renderers.BackgroundFill(color);
+      expect(circle.render()).toEqual(
+        '\n<rect width=100% height=100% fill="magenta" />\n'
+      );
+    });
+  });
+});
